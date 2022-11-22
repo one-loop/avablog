8f6e7b0a2efe4f8643024098d8fdbe55612e5760
Saad <78092430+one-loop@users.noreply.github.com>
2022-11-22 13:14:38 +0000

    more debugging

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
-},
+  },
+}
 
 module.exports = nextConfig
