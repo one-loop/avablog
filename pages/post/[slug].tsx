2f364391113308ea86520d8d399d828a4ed99836
Saad <78092430+one-loop@users.noreply.github.com>
2022-11-22 12:39:58 +0000

    Redeveloped individual blog post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-
+import moment from 'moment';
 import { PostDetail, Categories, PostWidget, Author, Loader, Community } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
 
@@ -11,11 +11,63 @@ const PostDetails = ({ post }) => {
     return <Loader />;
   }
 
+  const getContentFragment = (index, text, obj, type) => {
+    let modifiedText = text;
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = (<b key={index}>{text}</b>);
+      }
+
+      if (obj.italic) {
+        modifiedText = (<em key={index}>{text}</em>);
+      }
+
+      if (obj.underline) {
+        modifiedText = (<u key={index}>{text}</u>);
+      }
+    }
+
+    switch (type) {
+      case 'heading-three':
+        return <h3 key={index} className="text-xl font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
+      case 'paragraph':
+        return <p key={index} className="mb-8">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
+      case 'heading-four':
+        return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
+      case 'image':
+        return (
+          <img
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
+
   return (
     <>
       <PostDetail post={post} />
-      <Author author={post.author} />
-      <Community />
+      <main className="post-main">
+        <div className="post-content">
+          {post.content.raw.children.map((typeObj, index) => {
+            const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+
+            return getContentFragment(index, children, typeObj, typeObj.type);
+          })}
+          <Community />
+        </div>
+        <aside className="author-sidebar">
+          <p>Posted by</p>
+          <Author author={post.author} />
+        </aside>
+      </main>
+      
     </>
   );
 };
